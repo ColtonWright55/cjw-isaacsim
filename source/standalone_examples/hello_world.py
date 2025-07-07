69c08589342a2c08d6a008b401edc0024ecae0e0
ColtonWright55 <wright.2135@buckeyemail.osu.edu>
2025-07-07 12:28:52 -0400

    adding own robot

--- source/standalone_examples/hello_world.py
+++ source/standalone_examples/hello_world.py
@@ -86,45 +86,58 @@ omni.kit.commands.execute(
     urdf_path= "my_assets/ur10/urdf/ur10.urdf",
     import_config=import_config,
 )
+base_prim = stage.GetPrimAtPath("/ur10/base_link")  # adjust for your URDF
+# UsdGeom.Xformable(base_prim).AddTranslateOp().Set(Gf.Vec3f(2.0, 0.0, 0.5))
+UsdGeom.Xformable(base_prim).MakeMatrixXform().SetTranslate(Gf.Vec3f(2.0, 0.0, 0.5))
+# camera_state = ViewportCameraState("/OmniverseKit_Persp")
+# camera_state.set_position_world(Gf.Vec3d(2.0, -2.0, 0.5), True)
+# camera_state.set_target_world(Gf.Vec3d(0.0, 0.0, 0.0), True)
 
-camera_state = ViewportCameraState("/OmniverseKit_Persp")
-camera_state.set_position_world(Gf.Vec3d(2.0, -2.0, 0.5), True)
-camera_state.set_target_world(Gf.Vec3d(0.0, 0.0, 0.0), True)
+# stage = omni.usd.get_context().get_stage()
+# scene = UsdPhysics.Scene.Define(stage, Sdf.Path("/physicsScene"))
+# scene.CreateGravityDirectionAttr().Set(Gf.Vec3f(0.0, 0.0, -1.0))
+# scene.CreateGravityMagnitudeAttr().Set(9.81)
 
-stage = omni.usd.get_context().get_stage()
-scene = UsdPhysics.Scene.Define(stage, Sdf.Path("/physicsScene"))
-scene.CreateGravityDirectionAttr().Set(Gf.Vec3f(0.0, 0.0, -1.0))
-scene.CreateGravityMagnitudeAttr().Set(9.81)
+# distantLight = UsdLux.DistantLight.Define(stage, Sdf.Path("/DistantLight"))
+# distantLight.CreateIntensityAttr(500)
 
-distantLight = UsdLux.DistantLight.Define(stage, Sdf.Path("/DistantLight"))
-distantLight.CreateIntensityAttr(500)
-
-stage = omni.usd.get_context().get_stage()
-
-PhysxSchema.PhysxArticulationAPI.Get(stage, "/ur10").CreateSolverPositionIterationCountAttr(64)
-PhysxSchema.PhysxArticulationAPI.Get(stage, "/ur10").CreateSolverVelocityIterationCountAttr(64)
-
-joint_1 = UsdPhysics.DriveAPI.Get(stage.GetPrimAtPath("/ur10/joints/shoulder_pan_joint"), "angular")
-joint_2 = UsdPhysics.DriveAPI.Get(stage.GetPrimAtPath("/ur10/joints/shoulder_lift_joint"), "angular")
-joint_3 = UsdPhysics.DriveAPI.Get(stage.GetPrimAtPath("/ur10/joints/elbow_joint"), "angular")
-joint_4 = UsdPhysics.DriveAPI.Get(stage.GetPrimAtPath("/ur10/joints/wrist_1_joint"), "angular")
-joint_5 = UsdPhysics.DriveAPI.Get(stage.GetPrimAtPath("/ur10/joints/wrist_2_joint"), "angular")
-joint_6 = UsdPhysics.DriveAPI.Get(stage.GetPrimAtPath("/ur10/joints/wrist_3_joint"), "angular")
-
-# Set the drive mode, target, stiffness, damping and max force for each joint
-set_drive_parameters(joint_1, "position", math.degrees(0), math.radians(1e8), math.radians(5e7))
-set_drive_parameters(joint_2, "position", math.degrees(0), math.radians(1e8), math.radians(5e7))
-set_drive_parameters(joint_3, "position", math.degrees(0), math.radians(1e8), math.radians(5e7))
-set_drive_parameters(joint_4, "position", math.degrees(0), math.radians(1e8), math.radians(5e7))
-set_drive_parameters(joint_5, "position", math.degrees(0), math.radians(1e8), math.radians(5e7))
-set_drive_parameters(joint_6, "position", math.degrees(0), math.radians(1e8), math.radians(5e7))
+# stage = omni.usd.get_context().get_stage()
 
 
+# PhysxSchema.PhysxArticulationAPI.Get(stage, "/ur10").CreateSolverPositionIterationCountAttr(64)
+# PhysxSchema.PhysxArticulationAPI.Get(stage, "/ur10").CreateSolverVelocityIterationCountAttr(64)
+
+# joint_1 = UsdPhysics.DriveAPI.Get(stage.GetPrimAtPath("/ur10/joints/shoulder_pan_joint"), "angular")
+# joint_2 = UsdPhysics.DriveAPI.Get(stage.GetPrimAtPath("/ur10/joints/shoulder_lift_joint"), "angular")
+# joint_3 = UsdPhysics.DriveAPI.Get(stage.GetPrimAtPath("/ur10/joints/elbow_joint"), "angular")
+# joint_4 = UsdPhysics.DriveAPI.Get(stage.GetPrimAtPath("/ur10/joints/wrist_1_joint"), "angular")
+# joint_5 = UsdPhysics.DriveAPI.Get(stage.GetPrimAtPath("/ur10/joints/wrist_2_joint"), "angular")
+# joint_6 = UsdPhysics.DriveAPI.Get(stage.GetPrimAtPath("/ur10/joints/wrist_3_joint"), "angular")
+
+# # Set the drive mode, target, stiffness, damping and max force for each joint
+# set_drive_parameters(joint_1, "position", math.degrees(0), math.radians(1e8), math.radians(5e7))
+# set_drive_parameters(joint_2, "position", math.degrees(0), math.radians(1e8), math.radians(5e7))
+# set_drive_parameters(joint_3, "position", math.degrees(0), math.radians(1e8), math.radians(5e7))
+# set_drive_parameters(joint_4, "position", math.degrees(0), math.radians(1e8), math.radians(5e7))
+# set_drive_parameters(joint_5, "position", math.degrees(0), math.radians(1e8), math.radians(5e7))
+# set_drive_parameters(joint_6, "position", math.degrees(0), math.radians(1e8), math.radians(5e7))
 
 
 
 
 
+status, import_config = omni.kit.commands.execute("URDFCreateImportConfig")
+import_config.merge_fixed_joints = False
+import_config.fix_base = True
+import_config.make_default_prim = True
+import_config.create_physics_scene = True
+omni.kit.commands.execute(
+    "URDFParseAndImportFile",
+    urdf_path= "my_assets/agf1/urdf/agf1.urdf",
+    import_config=import_config,
+)
+# stage = omni.usd.get_context().get_stage()
+# agf1_prim = stage.GetPrimAtPath("/agf1")
 
 
 
